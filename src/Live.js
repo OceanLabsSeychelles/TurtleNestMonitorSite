ac4a2b1b6746d022d18a43d2cbab80e4a692826b
Brett <brettmsmith@protonmail.com>
2022-09-29 20:37:12 -0700

    Navbar Styling

--- src/Live.js
+++ src/Live.js
@@ -23,7 +23,7 @@ export default function Live() {
     }, []);
 
     return (
-        <div className="App" style={Styles.BootstrapCenter}>
+        <div className="App" style={{...Styles.BootstrapCenter, height:"92vh",backgroundColor:"rgb(107,107,143)"}}>
             <Col xs={12} >
                     <Card className={'bg-light'} style={{padding:'1em',margin:'3em'}}>
                         <Card.Body style={{textAlign: 'center'}}>
