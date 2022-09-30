659d5ff11224400f21c9649b97e3be5a39753981
Brett <brettmsmith@protonmail.com>
2022-09-29 20:46:53 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
                     <img
                         src={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjobo.sc%2Fwp-content%2Fuploads%2F2019%2F03%2FNature_Seychelles_Logo_270px.png&f=1&nofb=1&ipt=416c280ffa82404a0523d9fe9bce58fcbd425f5f981786d606a672aea7188a22&ipo=images"}
                         width="65"
-                        height="65"
+                        height="50"
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
