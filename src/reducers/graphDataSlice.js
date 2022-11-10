cee81682b1afafb9062a8918d9b0ae58a486a5e4
Brett Smith <brettmsmith@protonmail.com>
2022-11-10 22:49:37 +0900

    Initial commit (#1)

--- src/reducers/graphDataSlice.js
+++ src/reducers/graphDataSlice.js
@@ -148,7 +148,7 @@ export const graphDataSlice = createSlice({
             state.humidity = [];
             rawData.forEach((point, index) => {
                 console.log(point);
-                if(point.data.motion ===1)return
+                if(point.date.motion === 1)return;
                 state.oxygen.push({ x: index, y: point.data.oxygen });
                 state.temperature.push({ x: index, y: point.data.temperature });
                 state.humidity.push({ x: index, y: point.data.humidity });
